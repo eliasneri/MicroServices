f763fecc5e816fe19e36e955d6cd0b4197395db0
Elias Neri <eliasneri@hotmail.com>
2022-11-19 18:44:08 -0300

    Feign Client

--- hr-payroll/src/main/java/microservices/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/microservices/hrpayroll/service/PaymentService.java
@@ -1,33 +1,36 @@
 package microservices.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import microservices.hrpayroll.entities.Payment;
 import microservices.hrpayroll.entities.Worker;
+import microservices.hrpayroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
+		
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
+		// Map<String, String> uriVariables = new HashMap<>();
+		// uriVariables.put("id", ""+workerId);
+		// Com o FeignClient não precisa utilizar essa MAP
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		try {
+			Worker worker = workerFeignClient.findById(workerId).getBody();
+			return new Payment(worker.getname(), worker.getDailyIncome(), days);
+			
+		} catch (RuntimeException e) {
+			System.out.println("-- ERRO: houve um erro ao fazer a requisição!!");
+			
+		}
 		
-		return new Payment(worker.getname(), worker.getDailyIncome(), days);
+		// MELHORAR ISSO PARA RETORNAR UMA MSG DE ERRO, COM CORPO 404
+		return null;
 	}
 	
 }
