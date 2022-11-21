626d8c74110a9f8e6aad0a2eecf6bba86261e57e
Elias Neri <eliasneri@hotmail.com>
2022-11-20 22:19:02 -0300

    Eureka Configuration Server and alter pom.xml <spring.cloud.version>Hoxton.SR12, old are Hoxton.SR8

--- EurekaServer/src/main/java/microservices/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/microservices/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,21 @@
 package microservices.eurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
+	private static Logger log = LoggerFactory.getLogger(EurekaServerApplication.class);
+	
 	public static void main(String[] args) {
+						
 		SpringApplication.run(EurekaServerApplication.class, args);
+		log.info("Started EUREKA SERVER!!! " );
 	}
 
 }
