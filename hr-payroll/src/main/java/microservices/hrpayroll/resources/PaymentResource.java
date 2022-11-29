5b7aa362d517473362323a8f109ef840523683b3
Elias Neri <eliasneri@hotmail.com>
2022-11-29 12:52:59 -0300

    pom.xml

--- hr-payroll/src/main/java/microservices/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/microservices/hrpayroll/resources/PaymentResource.java
@@ -2,8 +2,6 @@ package microservices.hrpayroll.resources;
 
 import java.io.Serializable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
