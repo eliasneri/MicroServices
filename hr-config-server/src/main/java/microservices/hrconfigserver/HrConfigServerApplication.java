46376ab16022a9c41752eca784f250d98d7148b8
Elias Neri <eliasneri@hotmail.com>
2023-01-15 12:11:55 -0300

    hr-config-server configurating

--- hr-config-server/src/main/java/microservices/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/microservices/hrconfigserver/HrConfigServerApplication.java
@@ -1,13 +1,27 @@
 package microservices.hrconfigserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HrConfigServerApplication {
 
+	private static Logger log = LoggerFactory.getLogger(HrConfigServerApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(HrConfigServerApplication.class, args);
+
+		try {
+			SpringApplication.run(HrConfigServerApplication.class, args);
+			log.info("HR-Config-Server STARTED SUCESS!!!");
+		} catch (RuntimeException e) {
+			log.info("ERRO!!!");
+			e.printStackTrace();
+		}
+
 	}
 
 }
