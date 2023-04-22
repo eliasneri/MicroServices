cda2d43680dd9f7447fb4e25526379f6b0bdf074
Elias Neri <eliasneri@hotmail.com>
2023-04-22 17:51:46 -0300

    Project Hr-Oauth created

---
+++ hr-oauth/src/main/java/microservices/hroauth/HrOauthApplication.java
@@ -0,0 +1,16 @@
+package microservices.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HrOauthApplication.class, args);
+
+		}
+	}
+
+
