c1a29c6a2c6b4606aadfdc6efd5dbdbaeaf74c44
Elias Neri <eliasneri@hotmail.com>
2023-04-28 15:04:44 -0300

    Login JWT Token

--- hr-oauth/src/main/java/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/hroauth/services/UserService.java
@@ -5,12 +5,15 @@ import hroauth.feignclients.UserFeignCliente;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 
 @Service
-public class UserService implements Serializable {
+public class UserService implements UserDetailsService, Serializable {
 
     private static Logger log = LoggerFactory.getLogger(UserService.class);
 
@@ -24,6 +27,17 @@ public class UserService implements Serializable {
             throw new IllegalArgumentException("Email not Found");
         }
         log.info("Email Found! : " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user =  userFeignCliente.findByEmail(userName).getBody(); // vai pegar a resposta que está dentro do Response Entity
+        if (user == null){
+            log.error("Email Not Found: " + userName);
+            throw new UsernameNotFoundException("Email not Found");
+        }
+        log.info("Email Found! : " + userName);
         return user;
     }
 }
